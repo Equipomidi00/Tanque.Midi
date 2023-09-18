9147ef14f17551cffa693909b7fca1fe81c3583d
Damian C. Jara <128423831+DamianCJara@users.noreply.github.com>
2023-09-18 16:04:06 -0300

    Midi puede morir
    Además de agregar la posibilidad de que Ebenizer Midi se pueda morir, tambien modifique los escript de Gravis y sus hijos para que puedan matar al viejo

--- Assets/Scripts/CharacterController/LifeSystemPlayer.cs
+++ Assets/Scripts/CharacterController/LifeSystemPlayer.cs
@@ -9,19 +9,13 @@ para el avatar del jugador.
 public class LifeSystemPlayer : MonoBehaviour
 {
     [SerializeField] private float life;
-    [SerializeField] private GameObject MenuGameOver;
-
-    [Header("Game object que contiene el escript Destroy")]
-    public Destroy destroid;
-
+  
     public void SetLife(float damage)
     {
         life -= damage;
         if (life <= 0)
         {
-            Cursor.visible = true;
-            destroid.destroid = true;
-            MenuGameOver.SetActive(true);
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/Enemy/GravisDamage.cs
+++ Assets/Scripts/Enemy/GravisDamage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damage : MonoBehaviour
+public class GravisDamage : MonoBehaviour
 {
     [SerializeField] private float damage;
 
@@ -12,7 +12,6 @@ public class Damage : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             other.GetComponent<LifeSystemPlayer>().SetLife(damage);
-            Destroy(gameObject);
         }
     }
 }
